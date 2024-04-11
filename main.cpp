b0af8193993eb41789227f0d8464f181a1f8dbb4
user <2360860468@qq.com>
2024-04-11 16:28:03 +0800

    完成了Lexer

--- main.cpp
+++ main.cpp
@@ -1,6 +1,13 @@
-#include <iostream>
+#include "Lexer.h"
+using namespace fishDB;
+char sql[] = "select * from test where id >= 1200;";
+
 
 int main() {
+    TokenList list;
+    Lexer::LexerAnalysis(list,sql);
+    Lexer::LexerPrint(list);
     std::cout << "Hello, World!" << std::endl;
+
     return 0;
 }
---
+++ utility.h
@@ -0,0 +1,28 @@
+//
+// Created by user on 4/8/24.
+//
+
+#ifndef FISHDB_UTILITY_H
+#define FISHDB_UTILITY_H
+
+#include <cassert>
+#include <cstring>
+#include <string>
+#include <vector>
+#include <iostream>
+#include <fstream>
+#include <sstream>
+#include <algorithm>
+#include <map>
+#include <unordered_map>
+#include <set>
+#include <unordered_set>
+#include <list>
+
+namespace ErrorCode {
+    static constexpr int UnknownError = -1;
+    static constexpr int Success = 0;
+}
+
+
+#endif //FISHDB_UTILITY_H
